6f00be94b661ec412688b7926778163b69d1fe60
Muuhammad-Mubarak <37171527+Muuhammad-Mubarak@users.noreply.github.com>
2019-05-04 22:13:37 +0500

    Controllers and  views

---
+++ medical_encyclopedia_DB62/Controllers/DiseasesController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using medical_encyclopedia_DB62.Models;
+
+namespace medical_encyclopedia_DB62.Controllers
+{
+    public class DiseasesController : Controller
+    {
+        private medical_encylopediaEntities db = new medical_encylopediaEntities();
+
+        // GET: Diseases
+        public ActionResult Index()
+        {
+            var diseases = db.Diseases.Include(d => d.Disease1).Include(d => d.Disease2);
+            return View(diseases.ToList());
+        }
+
+        // GET: Diseases/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Disease disease = db.Diseases.Find(id);
+            if (disease == null)
+            {
+                return HttpNotFound();
+            }
+            return View(disease);
+        }
+
+        // GET: Diseases/Create
+        public ActionResult Create()
+        {
+            ViewBag.Id = new SelectList(db.Diseases, "Id", "Disease_name");
+            ViewBag.Id = new SelectList(db.Diseases, "Id", "Disease_name");
+            return View();
+        }
+
+        // POST: Diseases/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Disease_name,Disease_category,Disease_symptoms")] Disease disease)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Diseases.Add(disease);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Id = new SelectList(db.Diseases, "Id", "Disease_name", disease.Id);
+            ViewBag.Id = new SelectList(db.Diseases, "Id", "Disease_name", disease.Id);
+            return View(disease);
+        }
+
+        // GET: Diseases/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Disease disease = db.Diseases.Find(id);
+            if (disease == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Id = new SelectList(db.Diseases, "Id", "Disease_name", disease.Id);
+            ViewBag.Id = new SelectList(db.Diseases, "Id", "Disease_name", disease.Id);
+            return View(disease);
+        }
+
+        // POST: Diseases/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Disease_name,Disease_category,Disease_symptoms")] Disease disease)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(disease).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Id = new SelectList(db.Diseases, "Id", "Disease_name", disease.Id);
+            ViewBag.Id = new SelectList(db.Diseases, "Id", "Disease_name", disease.Id);
+            return View(disease);
+        }
+
+        // GET: Diseases/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Disease disease = db.Diseases.Find(id);
+            if (disease == null)
+            {
+                return HttpNotFound();
+            }
+            return View(disease);
+        }
+
+        // POST: Diseases/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Disease disease = db.Diseases.Find(id);
+            db.Diseases.Remove(disease);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
