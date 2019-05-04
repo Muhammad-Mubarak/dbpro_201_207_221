0c4578f5604958c9cbd1cd3d142c3cea6f54ca3d
Muuhammad-Mubarak <37171527+Muuhammad-Mubarak@users.noreply.github.com>
2019-05-04 21:15:32 +0500

    new project

---
+++ medical_encyclopedia_DB62/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(medical_encyclopedia_DB62.Startup))]
+namespace medical_encyclopedia_DB62
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
